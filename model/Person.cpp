ab8ac1b04e3fc93dfdc174b1ef4181fc61c462ff
Ahmed Osama <ahmedosamaft@gmail.com>
2023-09-19 16:07:39 +0300

    Adding Person Class

---
+++ model/Person.cpp
@@ -0,0 +1,52 @@
+//
+// Created by andro on 9/19/2023.
+//
+
+#include "Person.h"
+
+namespace Model {
+    long long int Person::getId() const {
+        return id;
+    }
+    void Person::setId(long long int id) {
+        Person::id = id;
+    }
+    const std::string &Person::getFirstName() const {
+        return first_name;
+    }
+    void Person::setFirstName(const std::string &firstName) {
+        first_name = firstName;
+    }
+    const std::string &Person::getLastName() const {
+        return last_name;
+    }
+    void Person::setLastName(const std::string &lastName) {
+        last_name = lastName;
+    }
+    const std::string &Person::getUserName() const {
+        return userName;
+    }
+    void Person::setUserName(const std::string &userName) {
+        Person::userName = userName;
+    }
+    const std::string &Person::getPassword() const {
+        return password;
+    }
+    void Person::setPassword(const std::string &password) {
+        Person::password = password;
+    }
+    Person::Person(long long int id, const std::string &firstName, const std::string &lastName, const std::string &userName, const std::string &password) : id(id), first_name(firstName), last_name(lastName), userName(userName), password(password) {}
+    /// read Entity from DB
+    Person::Person(std::string &line) {
+    }
+    // TODO: return Entity as string
+    std::string Person::ToSting() {
+        return std::string();
+    }
+    bool Person::operator==(const Person &rhs) const {
+        return id == rhs.id;
+    }
+    bool Person::operator==(const long long &id) const {
+        return (this->id == id);
+    }
+}// namespace Model
---
+++ model/Person.h
@@ -0,0 +1,42 @@
+//
+// Created by andro on 9/19/2023.
+//
+
+#ifndef BANK_SYSTEM_PERSON_H
+#define BANK_SYSTEM_PERSON_H
+
+#include <string>
+namespace Model {
+
+    class Person {
+    private:
+        long long id;
+        std::string first_name;
+        std::string last_name;
+        std::string userName;
+        std::string password;
+
+
+    public:
+        Person(long long int id, const std::string &firstName, const std::string &lastName, const std::string &userName, const std::string &password);
+        Person(std::string &line);
+        std::string ToSting();
+        bool operator==(const Person &rhs) const;
+        bool operator==(const long long &id) const;
+
+    public:
+        long long int getId() const;
+        void setId(long long int id);
+        const std::string &getFirstName() const;
+        void setFirstName(const std::string &firstName);
+        const std::string &getLastName() const;
+        void setLastName(const std::string &lastName);
+        const std::string &getUserName() const;
+        void setUserName(const std::string &userName);
+        const std::string &getPassword() const;
+        void setPassword(const std::string &password);
+    };
+
+}// namespace Model
+
+#endif//BANK_SYSTEM_PERSON_H
