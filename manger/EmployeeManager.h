d023259f96b87448c5af83ed571bbb46f31560b8
Mohamed Tamer <m.tamer16902@gmail.com>
2023-09-19 23:15:50 +0300

    modifying EmployeeManager

--- manger/EmployeeManager.h
+++ manger/EmployeeManager.h
@@ -8,8 +8,23 @@
 #define INC_5FDA1EAC732F499A94DC5EF107D62D97
 
 
-class EmployeeManager {
-};
+#include "../model/Employee.h"
+#include <vector>
+#include <map>
+#include <memory>
+namespace Manger {
+    using namespace std;
+    class EmployeeManger {
+    public:
+        static vector<shared_ptr<Model::Employee>> all_clients;
+        static string Employee_dir;
+        static shared_ptr<Model::Employee> current_client;
+        static map<long long,shared_ptr<Model::Employee>> id_client;
+        static void readEmployees();
+
+    };
+}
+
 // Todo: Valid Employee (user, password) : bool
 // Todo: Get Employee (user, password) : void
 // Todo: TakeControl(user, password) : void
