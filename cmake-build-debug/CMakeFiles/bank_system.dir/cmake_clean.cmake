b482b8acf3e28c6ffd87661ff11047ad1e0b0290
Mohamed Abdelhady <mohamedabdelhady9937@gmail.com>
2023-09-19 18:25:39 +0300

    nothing

---
+++ cmake-build-debug/CMakeFiles/bank_system.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank_system.dir/main.cpp.obj"
+  "CMakeFiles/bank_system.dir/main.cpp.obj.d"
+  "CMakeFiles/bank_system.dir/model/Client.cpp.obj"
+  "CMakeFiles/bank_system.dir/model/Client.cpp.obj.d"
+  "CMakeFiles/bank_system.dir/model/Person.cpp.obj"
+  "CMakeFiles/bank_system.dir/model/Person.cpp.obj.d"
+  "bank_system.exe"
+  "bank_system.exe.manifest"
+  "bank_system.pdb"
+  "libbank_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bank_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- model/Client.cpp
+++ model/Client.cpp
@@ -4,4 +4,5 @@
 
 #include "Client.h"
 namespace Model {
+
 }
